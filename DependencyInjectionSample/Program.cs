17fd27799fc2aab253fb3031a6b0003b396cf0c8
Reza Mohseni <mohsenireza.me@gmail.com>
2019-09-18 19:41:55 +0430

    Initial commit

---
+++ DependencyInjectionSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DependencyInjectionSample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var logger = new Logger();
+            var student = new Student(logger);
+            student.Register();
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        private ILogger Logger;
+
+        public Student(ILogger logger)
+        {
+            this.Logger = logger;
+        }
+
+        public void Register()
+        {
+            Logger.Log("Student was registered...");
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class Logger: ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
